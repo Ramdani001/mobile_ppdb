fbff650f7fb8f59454572df778b501be78a711cf
Rizkan00 <rizkan0990@gmail.com>
2024-08-17 00:31:51 +0700

    F

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {  ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
+const image = { uri: "../assets/bg.JPG" };
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} style={styles.container}>
+        <StatusBar style="light" />
+
+        <View style={styles.form}>
+
+
+          <TextInput style={styles.input} placeholder='Email Address'>
+          </TextInput>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +21,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    margin: 0,
+    // backgroundColor: '#41526e30',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  form:{
+    margin: 20,
+    padding: 20,
+    backgroundColor: '#fff',
+    width: 300,
+    height: 400,
+    borderRadius: 20,
+  },
+
+  input: {
+    width: 240,
+    marginTop: 30,
+    borderBottomWidth: 1,
+  }
+
 });
